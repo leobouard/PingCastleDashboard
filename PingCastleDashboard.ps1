b518f6f563b6e56b5577632065f205665ac6e205
Léo Bouard <leo.bouard@hotmail.com>
2024-02-19 20:10:37 +0100

    Ajout de plein de trucs

--- PingCastleDashboard.ps1
+++ PingCastleDashboard.ps1
@@ -1,12 +1,20 @@
-#Requires -Version 5.1
+﻿#Requires -Version 5.1
 #Requires -Modules @{ModuleName='PSWriteHTML';ModuleVersion='1.17.0'}
+
+
+$PSDefaultParameterValues = @{
+    'New-HtmlSection:HeaderBackGroundColor' = '#3D3834'
+    'New-HtmlSection:HeaderTextSize'        = '16'
+    'New-ChartBar:Color'                    = '#783CBD'
+    'New-ChartLine:Color'                   = '#783CBD'
+}
 
 $xmlFiles = Get-ChildItem -Path .\xml -Filter '*.xml'
 
 $reports = $xmlFiles | ForEach-Object {
 
     $domain = (Select-Xml -Path $_.FullName -XPath '/HealthcheckData/DomainFQDN').Node.'#text'
-    $date   = Get-Date (Select-Xml -Path $_.FullName -XPath '/HealthcheckData/GenerationDate').Node.'#text'
+    $date = Get-Date (Select-Xml -Path $_.FullName -XPath '/HealthcheckData/GenerationDate').Node.'#text'
 
     $xpath = '/HealthcheckData/RiskRules/HealthcheckRiskRule'
     $riskRules = (Select-Xml -Path $_.FullName -XPath $xpath).Node
@@ -35,109 +43,137 @@ $reports = $xmlFiles | ForEach-Object {
 }
 
 $reports = $reports | Sort-Object Date
+$allRiskRules = $reports.RiskRules | Sort-Object -Unique -Property RiskId
 
-New-Html -Name 'PingCastle dashboard' -FilePath '.\dashboard.html' -Show {
+# Create one dashboard foreach domain
+$reports.Domain | Sort-Object -Unique | ForEach-Object {
 
-    # Main page
-    New-HtmlTab -Name 'Main page' {
-
-        $chartAxisX = $reports | ForEach-Object { Get-Date $_.date -Format 'yyyy-MM-dd HH:mm' }
-        $chartLineTotal = $reports | ForEach-Object { ($_.RiskRules.Points | Measure-Object -Sum).Sum }
-        $chartLineAnoma = $reports | ForEach-Object { (($_.RiskRules | Where-Object {$_.Category -eq 'Anomalies'}).Points | Measure-Object -Sum).Sum }
-        $chartLinePrivi = $reports | ForEach-Object { (($_.RiskRules | Where-Object {$_.Category -eq 'PrivilegedAccounts'}).Points | Measure-Object -Sum).Sum }
-        $chartLineStale = $reports | ForEach-Object { (($_.RiskRules | Where-Object {$_.Category -eq 'StaleObjects'}).Points | Measure-Object -Sum).Sum }
-        $chartLineTrust = $reports | ForEach-Object { (($_.RiskRules | Where-Object {$_.Category -eq 'Trusts'}).Points | Measure-Object -Sum).Sum }
-
-        New-HtmlSection {
-            New-HTMLChart -Title 'Total cumulated points' {
-                New-ChartAxisX -Name $chartAxisX
-                New-ChartLine -Value $chartLineTotal -Name 'Point(s)'
-            }
-        }
-        New-HtmlSection {
-            New-HTMLPanel {
-                New-HTMLChart -Title 'Anomalies' {
-                    New-ChartAxisX -Name $chartAxisX
-                    New-ChartLine -Value $chartLineAnoma -Name 'Point(s)'
-                }
-                New-HTMLChart -Title 'Privileged Accounts' {
-                    New-ChartAxisX -Name $chartAxisX
-                    New-ChartLine -Value $chartLinePrivi -Name 'Point(s)'
-                }
-            }
-            New-HTMLPanel {
-                New-HTMLChart -Title 'Stale Objects' {
-                    New-ChartAxisX -Name $chartAxisX
-                    New-ChartLine -Value $chartLineStale -Name 'Point(s)'
-                }
-                New-HTMLChart -Title 'Trusts' {
-                    New-ChartAxisX -Name $chartAxisX
-                    New-ChartLine -Value $chartLineTrust -Name 'Point(s)'
-                }
-            }
-        }
-
-    }
-
-    # Initial situation
-    New-HtmlTab -Name 'Initial situation' {
-
-        $scores = $reports[0].Scores
-        New-HTMLSection {
-            New-HTMLGage -Label 'Anomalies' -MinValue 0 -MaxValue 100 -Value $scores.Anomaly
-            New-HTMLGage -Label 'Privileged Accounts' -MinValue 0 -MaxValue 100 -Value $scores.PrivilegiedGroup
-            New-HTMLGage -Label 'Stale Objects' -MinValue 0 -MaxValue 100 -Value $scores.StaleObjects
-            New-HTMLGage -Label 'Trusts' -MinValue 0 -MaxValue 100 -Value $scores.Trust
-        }
-        New-HTMLTable -Title 'Comparison with latest report' -DataTable $reports[0].RiskRules,$reports[-1].RiskRules -Compare -HighlightDifferences
-        New-HTMLTable -Title 'All risk rules' -DataTable $reports[0].RiskRules -DefaultSortIndex 0
-    }
-
-    # Create a new tab for all other reports
-    $i = 0
-    $reports | Select-Object -Skip 1 | ForEach-Object {
-
-        New-HtmlTab -Name (Get-Date $_.date -Format 'yyyy-MM-dd HH:mm') {
-
-            $currentReport  = $_
-            $previousReport = $reports[$i]
-
-            New-HtmlSection {
-                New-HtmlChart -Title 'Anomalies' {
-                    New-ChartBarOptions -Vertical
-                    New-ChartBar -Name 'Previous' -Value $previousReport.Scores.Anomaly
-                    New-ChartBar -Name 'Current' -Value $currentReport.Scores.Anomaly 
-                }
-                New-HtmlChart -Title 'Privileged Accounts' {
-                    New-ChartBarOptions -Vertical
-                    New-ChartBar -Name 'Previous' -Value $previousReport.Scores.PrivilegiedGroup
-                    New-ChartBar -Name 'Current' -Value $currentReport.Scores.PrivilegiedGroup 
-                }
-                New-HtmlChart -Title 'Stale Objects' {
-                    New-ChartBarOptions -Vertical
-                    New-ChartBar -Name 'Previous' -Value $previousReport.Scores.StaleObjects
-                    New-ChartBar -Name 'Current' -Value $currentReport.Scores.StaleObjects 
-                }
-                New-HtmlChart -Title 'Trusts' {
-                    New-ChartBarOptions -Vertical
-                    New-ChartBar -Name 'Previous' -Value $previousReport.Scores.Trust
-                    New-ChartBar -Name 'Current' -Value $currentReport.Scores.Trust 
-                }
-            }
-
-            # Comparison between current report and previous one
-            $comp = Compare-Object -ReferenceObject $previousReport.RiskRules -DifferenceObject $currentReport.RiskRules
-            $old = ($comp | Where-Object {$_.SideIndicator -eq '=>'}).InputObject | Select-Object Points,Category,Model,RiskId,Rationale
-            $new = ($comp | Where-Object {$_.SideIndicator -eq '<='}).InputObject | Select-Object Points,Category,Model,RiskId,Rationale
+    $domain = $_
+    $domainReports = $reports | Where-Object { $_.Domain -eq $domain }
+    New-Html -Name 'PingCastle dashboard' -FilePath ".\output\dashboard_$domain.html" -Encoding UTF8 -Author 'Léo Bouard' -Show {
         
-            # The following risk rules have been resolved since the last report (improvement)
-            New-HTMLTable -Title 'Risk rules resolved' -DataTable $old -DefaultSortIndex 0
-            # The following risk rules have been discovered since the last report (deterioration)
-            New-HTMLTable -Title 'New risk rules triggered' -DataTable $new -DefaultSortIndex 0
-        
+        # Header
+        New-HTMLHeader -HTMLContent { $ExecutionContext.InvokeCommand.ExpandString([string](Get-Content -Path '.\html\header.html')) }
+
+        # Main page tab
+        New-HtmlTab -Name 'Main page' -IconSolid home {
+
+            $chartAxisX = $domainReports | ForEach-Object { Get-Date $_.date -Format 'yyyy-MM-dd HH:mm' }
+            $chartLineTotal = $domainReports | ForEach-Object { ($_.RiskRules.Points | Measure-Object -Sum).Sum }
+            $chartLineAnoma = $domainReports | ForEach-Object { (($_.RiskRules | Where-Object { $_.Category -eq 'Anomalies' }).Points | Measure-Object -Sum).Sum }
+            $chartLinePrivi = $domainReports | ForEach-Object { (($_.RiskRules | Where-Object { $_.Category -eq 'PrivilegedAccounts' }).Points | Measure-Object -Sum).Sum }
+            $chartLineStale = $domainReports | ForEach-Object { (($_.RiskRules | Where-Object { $_.Category -eq 'StaleObjects' }).Points | Measure-Object -Sum).Sum }
+            $chartLineTrust = $domainReports | ForEach-Object { (($_.RiskRules | Where-Object { $_.Category -eq 'Trusts' }).Points | Measure-Object -Sum).Sum }
+
+            New-HtmlSection -HeaderText 'Evolution of global score' {
+                New-HTMLChart {
+                    New-ChartAxisX -Name $chartAxisX
+                    New-ChartLine -Value $chartLineTotal -Name 'Point(s)'
+                }
+            }
+            New-HtmlSection -HeaderText 'Evolution per category' {
+                New-HTMLPanel {
+                    New-HTMLChart -Title 'Anomalies' {
+                        New-ChartAxisX -Name $chartAxisX
+                        New-ChartLine -Value $chartLineAnoma -Name 'Point(s)'
+                    }
+                    New-HTMLChart -Title 'Privileged Accounts' {
+                        New-ChartAxisX -Name $chartAxisX
+                        New-ChartLine -Value $chartLinePrivi -Name 'Point(s)'
+                    }
+                }
+                New-HTMLPanel {
+                    New-HTMLChart -Title 'Stale Objects' {
+                        New-ChartAxisX -Name $chartAxisX
+                        New-ChartLine -Value $chartLineStale -Name 'Point(s)'
+                    }
+                    New-HTMLChart -Title 'Trusts' {
+                        New-ChartAxisX -Name $chartAxisX
+                        New-ChartLine -Value $chartLineTrust -Name 'Point(s)'
+                    }
+                }
+            }
         }
 
-        $i++
-    }
+        # Create a new tab for all other reports
+        $i = 0
+        $domainReports | Select-Object -Skip 1 | ForEach-Object {
+            
+            $currentReport = $_
+            $previousReport = $domainReports[$i]
 
+            $comp = Compare-Object -ReferenceObject $previousReport.RiskRules.RiskId -DifferenceObject $currentReport.RiskRules.RiskId
+            $riskSolved = $comp | Where-Object {$_.SideIndicator -eq '<='} | ForEach-Object {
+                $riskId = $_.InputObject
+                $allRiskRules | Where-Object {$_.RiskId -eq $riskId}
+            }
+            $riskNew = $comp | Where-Object {$_.SideIndicator -eq '=>'} | ForEach-Object {
+                $riskId = $_.InputObject
+                $allRiskRules | Where-Object {$_.RiskId -eq $riskId}
+            }
+
+            $i++
+
+            New-HtmlTab -Name (Get-Date $_.date -Format 'yyyy-MM-dd HH:mm') {
+
+                # Show PingCastle scores between 0 and 100 pts
+                New-HTMLSection -HeaderText 'Scores' {
+                    New-HTMLGage -Label 'Anomalies' -MinValue 0 -MaxValue 100 -Value $currentReport.Scores.Anomaly
+                    New-HTMLGage -Label 'Privileged Accounts' -MinValue 0 -MaxValue 100 -Value $currentReport.Scores.PrivilegiedGroup
+                    New-HTMLGage -Label 'Stale Objects' -MinValue 0 -MaxValue 100 -Value $currentReport.Scores.StaleObjects
+                    New-HTMLGage -Label 'Trusts' -MinValue 0 -MaxValue 100 -Value $currentReport.Scores.Trust
+                }
+
+                # Show evolution per item between initial, previous and current report
+                New-HtmlSection -HeaderText 'Comparison with previous reports' {
+                    New-HtmlChart -Title 'Anomalies' {
+                        New-ChartBarOptions -Vertical
+                        New-ChartBar -Name 'Initial' -Value (($domainReports[0].RiskRules | Where-Object { $_.Category -eq 'Anomalies' }).Points | Measure-Object -Sum).Sum
+                        New-ChartBar -Name 'Previous' -Value (($previousReport.RiskRules | Where-Object { $_.Category -eq 'Anomalies' }).Points | Measure-Object -Sum).Sum
+                        New-ChartBar -Name 'Current' -Value (($currentReport.RiskRules | Where-Object { $_.Category -eq 'Anomalies' }).Points | Measure-Object -Sum).Sum
+                    }
+                    New-HtmlChart -Title 'Privileged Accounts' {
+                        New-ChartBarOptions -Vertical
+                        New-ChartBar -Name 'Initial' -Value (($domainReports[0].RiskRules | Where-Object { $_.Category -eq 'PrivilegedAccounts' }).Points | Measure-Object -Sum).Sum
+                        New-ChartBar -Name 'Previous' -Value (($previousReport.RiskRules | Where-Object { $_.Category -eq 'PrivilegedAccounts' }).Points | Measure-Object -Sum).Sum
+                        New-ChartBar -Name 'Current' -Value (($currentReport.RiskRules | Where-Object { $_.Category -eq 'PrivilegedAccounts' }).Points | Measure-Object -Sum).Sum
+                    }
+                    New-HtmlChart -Title 'Stale Objects' {
+                        New-ChartBarOptions -Vertical
+                        New-ChartBar -Name 'Initial' -Value (($domainReports[0].RiskRules | Where-Object { $_.Category -eq 'StaleObjects' }).Points | Measure-Object -Sum).Sum
+                        New-ChartBar -Name 'Previous' -Value (($previousReport.RiskRules | Where-Object { $_.Category -eq 'StaleObjects' }).Points | Measure-Object -Sum).Sum
+                        New-ChartBar -Name 'Current' -Value (($currentReport.RiskRules | Where-Object { $_.Category -eq 'StaleObjects' }).Points | Measure-Object -Sum).Sum
+                    }
+                    New-HtmlChart -Title 'Trusts' {
+                        New-ChartBarOptions -Vertical
+                        New-ChartBar -Name 'Initial' -Value (($domainReports[0].RiskRules | Where-Object { $_.Category -eq 'Trusts' }).Points | Measure-Object -Sum).Sum
+                        New-ChartBar -Name 'Previous' -Value (($previousReport.RiskRules | Where-Object { $_.Category -eq 'Trusts' }).Points | Measure-Object -Sum).Sum
+                        New-ChartBar -Name 'Current' -Value (($currentReport.RiskRules | Where-Object { $_.Category -eq 'Trusts' }).Points | Measure-Object -Sum).Sum
+                    }
+                }
+
+                # Comparison between previous and current report
+                New-HtmlSection -HeaderText 'Improvement & deterioration' {
+                    New-HtmlPanel {
+                        # The following risk rules have been resolved since the last report (improvement)
+                        New-HTMLHeading h3 -HeadingText 'Risk rules resolved'
+                        New-HTMLTable -DataTable $riskSolved -DefaultSortIndex 1 -HideButtons
+                    }
+                    New-HTMLPanel {
+                        # The following risk rules have been discovered since the last report (deterioration)
+                        New-HTMLHeading h3 -HeadingText 'New risk rules triggered'
+                        New-HTMLTable -DataTable $riskNew -DefaultSortIndex 1 -HideButtons
+                    }
+                }
+
+                # Show all risk rules
+                New-HtmlSection -HeaderText 'All current risk rules' {
+                    New-HTMLTable -DataTable $currentReport.RiskRules -DefaultSortIndex 1
+                }
+            }
+        }
+
+        # Footer
+        New-HTMLFooter -HTMLContent { $ExecutionContext.InvokeCommand.ExpandString([string](Get-Content -Path '.\html\footer.html')) }
+    }
 }
--- readme.md
+++ readme.md
@@ -1,8 +1,10 @@
 # PingCastleDashboard
 
-Génère un tableau de bord sur l'avancée du score Ping Castle à travers le temps en se basant sur les fichiers XML générés par Ping Castle.
+Génère un tableau de bord sur l'avancée du score PingCastle à travers le temps en se basant sur les fichiers XML générés par PingCastle.
 
 Pour l'instant je me sert du `README.md` comme une liste des trucs à faire 😊
+
+Pour se servir du script, c'est très simple : on balance tous les fichiers XML issus de PingCastle (vous pouvez mélanger les domaines, y'a pas de soucis) puis vous executez le script `PingCastleDashboard.ps1` et le tour est joué !
 
 ## Choses à faire
 
@@ -16,7 +18,7 @@ Pour l'instant je me sert du `README.md` comme une liste des trucs à faire 😊
 1. Page principale
   - Courbe avec l'évolution du nombre de points au total et sur chaque catégorie
 2. Report #01
-  - Informations en bref : numéro de version, date de l'audit, etc
+  - Informations en bref : numéro de version de PingCastle, date de l'audit, etc
   - Evolution et points gagnés entre le dernier rapport et la situation initiale
 3. Report #02 à #999
   - Informations en bref : numéro de version, date de l'audit, etc
