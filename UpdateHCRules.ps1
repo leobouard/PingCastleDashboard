fe00a4814811e844269f83bad2b64d12fd641aec
Léo Bouard <leo.bouard@hotmail.com>
2026-02-09 10:04:37 +0100

    Update UpdateHCRules.ps1

--- UpdateHCRules.ps1
+++ UpdateHCRules.ps1
@@ -1,4 +1,4 @@
-﻿$uri = "https://api.github.com/repos/netwrix/pingcastle/contents/PingCastle/Healthcheck/Rules?ref=master"
+﻿$uri = "https://api.github.com/repos/netwrix/pingcastle/contents/PingCastleCommon/Healthcheck/Rules?ref=master"
 $results = Invoke-RestMethod -Method GET -Uri $uri | Where-Object { $_.Name -like '*.cs' }
 
 $i = 0
